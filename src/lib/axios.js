eea626fea01b68e5550ae2c833de616280e10b8f
Deep Guchhait <deepguchhait01@gmail.com>
2026-06-23 01:42:36 +0530

    fix bugs

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:8000/api":"/api"
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const axiosInstance=axios.create({
-    baseURL:BASE_URL,
-    withCredentials:true,
-})
+if (!BASE_URL) {
+  console.warn("VITE_BASE_URL is not set");
+}
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL || "http://localhost:5000/api",
+  withCredentials: true,
+});
